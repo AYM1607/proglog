6762591ea259490081e892101bf47177cd5b257c
AYM1607 <u.g.a.mariano@gmail.com>
2021-07-30 20:12:18 -0500

    Finished store

---
+++ internal/log/store_test.go
@@ -0,0 +1,20 @@
+package log
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write) + lenWidth)
+)
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+	}
+}
